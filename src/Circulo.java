7327bb5c121fd999b4088fccb5891b653c850808
Doiae9 <leonardogh75@gmail.com>
2022-03-04 11:56:57 -0600

    Creación de constructores

--- src/Circulo.java
+++ src/Circulo.java
@@ -2,4 +2,18 @@ public class Circulo {
     private float area;
     private float diametro;
     private float Circun;
+    private float radio;
+
+    public float getRadio() {
+        return radio;
+    }
+public Circulo(float radio){
+        this.radio=radio;
+}
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+    public Circulo() {
+        System.out.println("No sirvo para nada");
+    }
 }
