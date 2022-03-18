76f2ecc2c537b7a961d641258d2a0a39c06461fc
Doiae9 <leonardogh75@gmail.com>
2022-03-18 11:13:15 -0600

    Corrección completa

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,14 +1,15 @@
 public class Circulo {
-    private float dia;
-    private float Circun;
+    //atributo
     private float radio;
 
     public float getRadio() {
         return radio;
     }
-public Circulo(float radio){
-        this.radio=radio;
-}
+
+    public Circulo(float radio) {
+        this.radio = radio;
+    }
+
     public void setRadio() {
         this.radio = radio;
 
@@ -17,18 +18,35 @@ public Circulo(float radio){
     public Circulo() {
         System.out.println("No sirvo para nada");
     }
-    public void diametro(){
-        dia=radio*2;
-    }
-    public void Area(){
+    //metodos
+    //public float calculararea(){
+    //return (float) (Math.PI*Math.pow(radio, 2)); }
+    // public float calcularDiametro(){
+    //return radio*2; }
+    //public float calcularCircunferencia(){
+    //opcion1
+    //return (float) (Math.PI*radio*2);
+    //opcion 2
+    //return (float) (Math.PI*calcular Diametro())
+
+    public float CalcArea(){
         float area=0;
-        area= (float) (3.1416*(Math.pow(radio, 2)));
+        return (float) (Math.PI*Math.pow(radio, 2));
     }
-    public void Circunferencia(){
-        float circun=0;
-        this.dia=dia;
-        circun= (float) (3.141645*dia/(3.1416*radio*2));
+    public float Calcdiametro(){
+        return radio*2;
+    }
+    public float CalcCircunferencia(){
+    return (float) (Math.PI*radio*2);
+    }
+
+    @Override
+    public String toString() {
+        return "El Circulo tiene un radio de " +radio+
+                "\n El area del circulo es" +CalcArea()+
+                "\nEl diametro del ciculo es"+Calcdiametro()+"\n La circunferencia es"+CalcCircunferencia();
+
+    }
     }
 
 
-}
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        Circulo objetoCir= new Circulo();
-        System.out.println("A continuación se le pedirá ingresar el radio de un circulo");
+        Circulo objetoCir= new Circulo( 5.51f);
+        System.out.println(objetoCir);
         objetoCir.setRadio();
-        objetoCir.diametro();
-        objetoCir.Area();
-        objetoCir.Circunferencia();
+        objetoCir.Calcdiametro();
+        objetoCir.CalcArea();
+        objetoCir.CalcCircunferencia();
+//Circulo objetoCirculo= new Circulo(5.28);
+// .calcularDiametro()
 
+        //
 
+        }
     }
-}
+
